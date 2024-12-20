fe53c3a0a76f931c35f18affdd3f17bea1ff4b8a
AnilAntari <86964331+AnilAntari@users.noreply.github.com>
2024-12-20 12:29:33 +0300

    Created autodeb.sh (iperf3) for ALSE 1.8

---
+++ iperf3/ALSE1.8/autodeb.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+function sources_list {
+  cat << EOF > /etc/apt/sources.list
+deb https://dl.astralinux.ru/astra/stable/1.8_x86-64/main-repository/ 1.8_x86-64 main contrib non-free non-free-firmware
+deb https://dl.astralinux.ru/astra/stable/1.8_x86-64/extended-repository/ 1.8_x86-64 main contrib non-free non-free-firmware
+EOF
+
+  apt update
+}
+
+function add_chroot {
+  apt install debootstrap qemu-user-static -y
+  debootstrap --arch=amd64 --components=main,contrib,non-free,non-free-firmware 1.8_x86-64 chroot-amd64 https://dl.astralinux.ru/astra/stable/1.8_x86-64/main-repository/
+
+  chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cat << EOF > /etc/apt/sources.list
+deb https://dl.astralinux.ru/astra/stable/1.8_x86-64/main-repository/ 1.8_x86-64 main contrib non-free non-free-firmware
+deb https://dl.astralinux.ru/astra/stable/1.8_x86-64/extended-repository/ 1.8_x86-64 main contrib non-free non-free-firmware
+EOF"
+
+  echo "export LC_ALL=C" | chroot /home/"$SUDO_USER"/chroot-amd64 tee -a /root/.bashrc
+  chroot /home/"$SUDO_USER"/chroot-amd64 apt update
+  chroot /home/"$SUDO_USER"/chroot-amd64 apt install devscripts quilt equivs unzip wget -y
+}
+
+function install_iperf3 {
+	
+	dir="iperf3-3.18-1_amd64"
+	
+	chroot /home/"$SUDO_USER"/chroot-amd64 wget -P /opt https://github.com/esnet/iperf/releases/download/3.18/iperf-3.18.tar.gz
+	chroot /home/"$SUDO_USER"/chroot-amd64 tar -xzvf /opt/iperf-3.18.tar.gz -C /opt
+	chroot /home/"$SUDO_USER"/chroot-amd64 mkdir /opt/$dir/usr
+  chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cd /opt/iperf-3.18/ && ./configure --prefix=/opt/$dir/usr"
+	chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cd /opt/iperf-3.18/ && make" 
+	chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cd /opt/iperf-3.18/ && make check"
+	sleep 10
+	chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cd /opt/iperf-3.18/ && make install"
+	chroot /home/"$SUDO_USER"/chroot-amd64 mkdir -p /opt/$dir/usr/{lib/systemd/system,share/doc}
+	chroot /home/"$SUDO_USER"/chroot-amd64 cp /opt/iperf-3.18/README.md /opt/$dir/usr/share/doc
+	chroot /home/"$SUDO_USER"/chroot-amd64 chmod 644 /opt/$dir/usr/share/doc/README.md
+	chroot /home/"$SUDO_USER"/chroot-amd64 cp /opt/iperf-3.18/contrib/iperf3.service /opt/$dir/usr/lib/systemd/system
+	chroot /home/"$SUDO_USER"/chroot-amd64 chmod 644 /opt/$dir/usr/lib/systemd/system/iperf3.service
+	chroot /home/"$SUDO_USER"/chroot-amd64 mkdir /opt/$dir/DEBIAN
+	chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cat << EOF > /opt/$dir/DEBIAN/control
+Package: iperf3
+Version: 3.18-1
+Architecture: amd64
+Maintainer: AnilAntari
+Depends: debconf, adduser, libc6, debconf (>= 0.5) | debconf-2.0
+Section: net
+Priority: optional
+Homepage: https://github.com/esnet/iperf
+Description: Internet Protocol bandwidth measuring tool
+ Iperf3 is a tool for performing network throughput measurements. It can
+ test either TCP or UDP throughput.
+ .
+ This is a new implementation that shares no code with the original
+ iperf from NLANR/DAST and also is not backwards compatible.
+ .
+ This package contains the command line utility.
+EOF"
+	chroot /home/"$SUDO_USER"/chroot-amd64 dpkg-deb --build /opt/$dir
+	apt install ./chroot-amd64/opt/iperf3-3.18-1_amd64.deb -y
+}
+
+sources_list
+add_chroot
+install_iperf3
