f0829a157f650f49d0867156ae3f01910047c953
AnilAntari <retro201272@gmail.com>
2024-12-21 00:57:42 +0300

    Updating scripts

--- WinBox/ALSE1.7/autodeb.sh
+++ WinBox/ALSE1.7/autodeb.sh
@@ -1,4 +1,8 @@
-number=14
+#!/bin/bash
+
+set -e
+
+chroot_dir="chroot /home/"$SUDO_USER"/chroot-amd64"
 
 function sources_list {
   cat << EOF > /etc/apt/sources.list
@@ -13,25 +17,27 @@ EOF
 
 function add_chroot {
 
+  apt install debootstrap qemu-user-static -y
   debootstrap --arch=amd64 --components=main,contrib,non-free 1.7_x86-64 chroot-amd64 http://download.astralinux.ru/astra/stable/1.7_x86-64/repository-base/
 
-  chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cat << EOF > /etc/apt/sources.list
+  $chroot_dir bash -c "cat << EOF > /etc/apt/sources.list
 deb https://download.astralinux.ru/astra/stable/1.7_x86-64/repository-base/ 1.7_x86-64 main contrib non-free
 deb https://download.astralinux.ru/astra/stable/1.7_x86-64/repository-extended/ 1.7_x86-64 main contrib non-free
 EOF"
 
-  echo "export LC_ALL=C" | chroot /home/"$SUDO_USER"/chroot-amd64 tee -a /root/.bashrc
-  chroot /home/"$SUDO_USER"/chroot-amd64 apt update
-  chroot /home/"$SUDO_USER"/chroot-amd64 apt install devscripts quilt equivs unzip wget -y
+  $chroot_dir bash -c "echo \"export LC_ALL=C\" >> /root/.bashrc"
+  $chroot_dir apt update
+  $chroot_dir apt install devscripts quilt equivs unzip wget build-essential -y
 }
 
 function install_winbox {
-  chroot /home/"$SUDO_USER"/chroot-amd64 wget -P /opt https://download.mikrotik.com/routeros/winbox/4.0beta14/WinBox_Windows.zip
-  chroot /home/"$SUDO_USER"/chroot-amd64 unzip /opt/WinBox_Windows.zip -d /opt/
-  chroot /home/"$SUDO_USER"/chroot-amd64 mkdir -p /opt/winbox4.0beta"$number"/usr/{share/pixmaps,share/applications,share/winbox,bin}
-  chroot /home/"$SUDO_USER"/chroot-amd64 cp /opt/WinBox.exe /opt/winbox4.0beta"$number"/usr/share/winbox
-  chroot /home/"$SUDO_USER"/chroot-amd64 cp /opt/assets/img/winbox.png /opt/winbox4.0beta"$number"/usr/share/pixmaps
-  chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cat << EOF > /opt/winbox4.0beta"$number"/usr/share/applications/winbox.desktop
+  version=14
+
+  $chroot_dir wget -P /opt https://download.mikrotik.com/routeros/winbox/4.0beta14/WinBox_Windows.zip
+  $chroot_dir mkdir /opt/winbox4.0beta"$version"
+  $chroot_dir mkdir -p /opt/winbox4.0beta"$version"/debian/winbox/usr/{share/pixmaps,share/applications,share/winbox,bin}
+  $chroot_dir unzip /opt/WinBox_Windows.zip -d /opt/winbox4.0beta"$version"
+  $chroot_dir bash -c "cat << EOF > /opt/winbox4.0beta"$version"/debian/winbox/usr/share/applications/winbox.desktop
 [Desktop Entry]
 Type=Application
 Version=1.0
@@ -44,7 +50,7 @@ Categories=Utility
 StartupWMClass=WinBox.exe
 EOF"
 
-  chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "echo '#!/bin/bash
+ $chroot_dir bash -c "echo '#!/bin/bash
 export WINEPREFIX=\"\$HOME\"/.winbox/wine
 export WINEARCH=win64
 export WINEDLLOVERRIDES=\"mscoree=\" # disable mono
@@ -54,30 +60,43 @@ if [ ! -d \"\$HOME\"/.winbox ] ; then
    wineboot -u
 fi
 
-wine /usr/share/winbox/WinBox.exe \"\$@\"' > /opt/winbox4.0beta"$number"/usr/bin/winbox"
+wine /usr/share/winbox/WinBox.exe \"\$@\"' > /opt/winbox4.0beta"$version"/debian/winbox/usr/bin/winbox"
 
-  chroot /home/"$SUDO_USER"/chroot-amd64 mkdir /opt/winbox4.0beta"$number"/DEBIAN
-  chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cat << EOF > /opt/winbox4.0beta"$number"/DEBIAN/control
-Package: winbox
-Version: 4.0beta"$number"-1
-Priority: optional
-Origin: WinBox
+  $chroot_dir bash -c "cat << EOF > /opt/winbox4.0beta"$version"/debian/control
+Source: winbox
 Section: Application/Tools
+Priority: optional
 Maintainer: AnilAntari
+Build-Depends: debhelper (>=9), build-essential
+
+Package: winbox
+Version: 4.0beta"$version"-1
+Origin: WinBox
 Depends: wine, wine-mono, wine-gecko
 Architecture: amd64
 Homepage: https://mikrotik.com/download
 Description: MikroTik RouterOS GUI Configurator (wine)
 EOF"
 
-  chroot /home/"$SUDO_USER"/chroot-amd64 chmod 755 /opt/winbox4.0beta"$number"/usr/share/winbox/WinBox.exe
-  chroot /home/"$SUDO_USER"/chroot-amd64 chmod 755 /opt/winbox4.0beta"$number"/usr/bin/winbox
-  chroot /home/"$SUDO_USER"/chroot-amd64 chmod 644 /opt/winbox4.0beta"$number"/usr/share/pixmaps/winbox.png
-  chroot /home/"$SUDO_USER"/chroot-amd64 chmod 644 /opt/winbox4.0beta"$number"/usr/share/applications/winbox.desktop
-  chroot /home/"$SUDO_USER"/chroot-amd64 dpkg-deb --build /opt/winbox4.0beta"$number"
-  chroot /home/"$SUDO_USER"/chroot-amd64 mv /opt/winbox4.0beta"$number".deb /opt/winbox_4.0beta"$number"-1_amd64.deb
+  $chroot_dir bash -c "echo \"9\" > /opt/winbox4.0beta"$version"/debian/compat"
 
-  apt install /home/"$SUDO_USER"/chroot-amd64/opt/winbox_4.0beta"$number"-1_amd64.deb
+  $chroot_dir bash -c "cat << EOF > /opt/winbox4.0beta"$version"/debian/changelog
+winbox (4.0beta$version-1) stretch; urgency=medium
+
+  * Creating a package with version 4.0beta"$version".
+
+  -- AnilAntari   $(date -R)
+EOF"
+
+  $chroot_dir bash -c "printf \"#!/usr/bin/make -f\n\n\" > /opt/winbox4.0beta\"$version\"/debian/rules"
+  $chroot_dir bash -c "printf \"clean:\n\t@# Do nothing\n\n\" >> /opt/winbox4.0beta\"$version\"/debian/rules"
+  $chroot_dir bash -c "printf \"build:\n\t@# Do nothing\n\n\" >> /opt/winbox4.0beta\"$version\"/debian/rules"
+  $chroot_dir bash -c "printf \"binary:\n\tmkdir -p debian/winbox/usr/share/pixmaps\n\tcp WinBox.exe debian/winbox/usr/share/winbox\n\tcp assets/img/winbox.png debian/winbox/usr/share/pixmaps\n\tchmod 755 debian/winbox/usr/share/winbox/WinBox.exe\n\tchmod 755 debian/winbox/usr/bin/winbox\n\tchmod 644 debian/winbox/usr/share/pixmaps/winbox.png\n\tchmod 644 debian/winbox/usr/share/applications/winbox.desktop\n\t\tdh_gencontrol\n\tdh_builddeb\n\" >> /opt/winbox4.0beta\"$version\"/debian/rules"
+
+  $chroot_dir << EOF
+cd /opt/winbox4.0beta"$version"
+dpkg-buildpackage -b
+EOF
 }
 
 sources_list
--- iperf3/ALSE1.7/autodeb.sh
+++ iperf3/ALSE1.7/autodeb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -e
+
+chroot_dir="chroot /home/"$SUDO_USER"/chroot-amd64"
+
 function sources_list {
   cat << EOF > /etc/apt/sources.list
 deb https://dl.astralinux.ru/astra/stable/1.7_x86-64/repository-main/ 1.7_x86-64 main contrib non-free
@@ -12,45 +16,40 @@ EOF
 }
 
 function add_chroot {
+
   apt install debootstrap qemu-user-static -y
   debootstrap --arch=amd64 --components=main,contrib,non-free 1.7_x86-64 chroot-amd64 http://download.astralinux.ru/astra/stable/1.7_x86-64/repository-base/
 
-  chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cat << EOF > /etc/apt/sources.list
+  $chroot_dir bash -c "cat << EOF > /etc/apt/sources.list
 deb https://download.astralinux.ru/astra/stable/1.7_x86-64/repository-base/ 1.7_x86-64 main contrib non-free
 deb https://download.astralinux.ru/astra/stable/1.7_x86-64/repository-extended/ 1.7_x86-64 main contrib non-free
 EOF"
 
-  echo "export LC_ALL=C" | chroot /home/"$SUDO_USER"/chroot-amd64 tee -a /root/.bashrc
-  chroot /home/"$SUDO_USER"/chroot-amd64 apt update
-  chroot /home/"$SUDO_USER"/chroot-amd64 apt install devscripts quilt equivs unzip wget -y
+  $chroot_dir bash -c "echo \"export LC_ALL=C\" >> /root/.bashrc"
+  $chroot_dir apt update
+  $chroot_dir apt install devscripts quilt equivs unzip wget build-essential -y
 }
 
+
 function install_iperf3 {
-	
-	dir="iperf3-3.18-1_amd64"
-	
-	chroot /home/"$SUDO_USER"/chroot-amd64 wget -P /opt https://github.com/esnet/iperf/releases/download/3.18/iperf-3.18.tar.gz
-	chroot /home/"$SUDO_USER"/chroot-amd64 tar -xzvf /opt/iperf-3.18.tar.gz -C /opt
-	chroot /home/"$SUDO_USER"/chroot-amd64 mkdir /opt/$dir/usr
-    chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cd /opt/iperf-3.18/ && ./configure --prefix=/opt/$dir/usr"
-	chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cd /opt/iperf-3.18/ && make" 
-	chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cd /opt/iperf-3.18/ && make check"
-	sleep 10
-	chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cd /opt/iperf-3.18/ && make install"
-	chroot /home/"$SUDO_USER"/chroot-amd64 mkdir -p /opt/$dir/usr/{lib/systemd/system,share/doc}
-	chroot /home/"$SUDO_USER"/chroot-amd64 cp /opt/iperf-3.18/README.md /opt/$dir/usr/share/doc
-	chroot /home/"$SUDO_USER"/chroot-amd64 chmod 644 /opt/$dir/usr/share/doc/README.md
-	chroot /home/"$SUDO_USER"/chroot-amd64 cp /opt/iperf-3.18/contrib/iperf3.service /opt/$dir/usr/lib/systemd/system
-	chroot /home/"$SUDO_USER"/chroot-amd64 chmod 644 /opt/$dir/usr/lib/systemd/system/iperf3.service
-	chroot /home/"$SUDO_USER"/chroot-amd64 mkdir /opt/$dir/DEBIAN
-	chroot /home/"$SUDO_USER"/chroot-amd64 bash -c "cat << EOF > /opt/$dir/DEBIAN/control
-Package: iperf3
-Version: 3.18-1
-Architecture: amd64
-Maintainer: AnilAntari
-Depends: debconf, adduser, libc6, debconf (>= 0.5) | debconf-2.0
+  version="3.18-1"
+  chroot_iperf="/opt/iperf-3.18"
+
+  $chroot_dir wget -P /opt https://github.com/esnet/iperf/releases/download/3.18/iperf-3.18.tar.gz
+  $chroot_dir tar -xzvf /opt/iperf-3.18.tar.gz -C /opt
+  $chroot_dir mkdir /opt/iperf-3.18/debian
+
+  $chroot_dir bash -c "cat << EOF > /opt/iperf-3.18/debian/control
+Source: iperf3
 Section: net
 Priority: optional
+Maintainer: AnilAntari
+Build-Depends: debhelper (>=9), build-essential
+
+Package: iperf3
+Version: $version
+Architecture: amd64
+Depends: debconf, adduser, libc6, debconf (>= 0.5) | debconf-2.0
 Homepage: https://github.com/esnet/iperf
 Description: Internet Protocol bandwidth measuring tool
  Iperf3 is a tool for performing network throughput measurements. It can
@@ -59,10 +58,27 @@ Description: Internet Protocol bandwidth measuring tool
  This is a new implementation that shares no code with the original
  iperf from NLANR/DAST and also is not backwards compatible.
  .
- This package contains the command line utility.
 EOF"
-	chroot /home/"$SUDO_USER"/chroot-amd64 dpkg-deb --build /opt/$dir
-	apt install ./chroot-amd64/opt/iperf3-3.18-1_amd64.deb -y
+
+  $chroot_dir bash -c "echo \"9\" > /opt/iperf-3.18/debian/compat"
+
+  $chroot_dir bash -c "cat << EOF > /opt/iperf-3.18/debian/changelog
+iperf3 ($version) stretch; urgency=medium
+
+  * Creating a package with version 3.18.
+
+  -- AnilAntari   $(date -R)
+EOF"
+
+  $chroot_dir bash -c "printf \"#!/usr/bin/make -f\n\n\" > /opt/iperf-3.18/debian/rules"
+  $chroot_dir bash -c "printf \"clean:\n\t@# Do nothing\n\n\" >> /opt/iperf-3.18/debian/rules"
+  $chroot_dir bash -c "printf \"build:\n\tmkdir -p debian/iperf3/usr\n\t./configure --prefix=/opt/iperf-3.18/debian/iperf3/usr\n\tmake\n\tmake check\n\tmake install\n\n\" >> /opt/iperf-3.18/debian/rules"
+  $chroot_dir bash -c "printf \"binary:\n\tmkdir -p debian/iperf3/usr/share/doc/iperf3\n\tmkdir -p debian/iperf3/usr/share/lib/systemd/system\n\tcp README.md debian/iperf3/usr/share/doc/iperf3\n\tchmod 0644 debian/iperf3/usr/share/doc/iperf3/README.md\n\tcp contrib/iperf3.service debian/iperf3/usr/share/lib/systemd/system\n\tchmod 0644 debian/iperf3/usr/share/lib/systemd/system\n\tdh_gencontrol\n\tdh_builddeb\n\" >> /opt/iperf-3.18/debian/rules"
+
+  $chroot_dir << EOF
+cd /opt/iperf-3.18
+dpkg-buildpackage -b
+EOF
 }
 
 sources_list
